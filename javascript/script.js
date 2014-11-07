4954cd56707b896891ecab4e96e4996905cd99f3
Dan Meehan <dan.meehan17@gmail.com>
2014-11-07 10:03:13 -0500

    Add content and styles

--- javascript/script.js
+++ javascript/script.js
@@ -4,8 +4,17 @@ $('.bubble').hover(function () {
   $(this).fadeTo(500, 1);
 });
 
-$('.bubble-text').hover(function () {
-  $(this).fadeTo(500, 1);
-}, function () {
-  $(this).fadeTo(500, 0);
+$('#bubble-me').click(function () {
+  $('#me').slideDown(500);
+  $('.content-text').not('#me').slideUp(500);
+});
+
+$('#bubble-school').click(function () {
+  $('#school').slideDown(500);
+  $('.content-text').not('#school').slideUp(500);
+});
+
+$('#bubble-work').click(function () {
+  $('#work').slideDown(500);
+  $('.content-text').not('#work').slideUp(500);
 });
