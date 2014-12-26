718a0debb6300f215b6e3dd67f8ce673d6254b7c
dmee3 <dan.meehan17@gmail.com>
2014-12-26 17:36:39 -0500

    Add test of spotify api

--- javascript/script.js
+++ javascript/script.js
@@ -28,3 +28,53 @@ $('#bubble-iMusic').click(function () {
   $('#iMusic').slideDown(500);
   $('.content-text').not('#iMusic').slideUp(500);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	var baseURI = 'https://api.spotify.com/v1/';
+    
+    request = function(query, params) {
+        ajax = new XMLHttpRequest();
+        url = buildURL(query, params);
+        
+        ajax.onreadystatechange = function() {
+            if (ajax.readyState == 4 && ajax.status == 200) {
+                document.getElementById('artist').innerHTML = ajax.responseText;
+            }
+        }
+        ajax.open('GET',url,true);
+        document.getElementById('artist').innerHTML=url;
+        ajax.send();
+    }
+    
+    buildURL = function(query, params) {
+        var url = baseURI + query + '?';
+        for (var k in params) {
+            url += encodeURIComponent(k) + '=' + encodeURIComponent(params[k]) + '&';
+        }
+        return url.substring(0, url.length - 1);
+    }
+    
+	search = function(query) {
+        var params = {
+            q:query,
+            type:'artist',
+            market:'US'
+        }
+        request('search', params);
+	}
