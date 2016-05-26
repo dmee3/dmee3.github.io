4c6a1106186514c7d25fc4971e1d954997d14846
Dan Meehan <dan.meehan@cuttercroix.com>
2016-05-26 11:13:19 -0400

    Refactoring in markdown engine

--- js/markdown.js
+++ js/markdown.js
@@ -4,12 +4,14 @@ var headers = { '#': 'h1', '##': 'h2', '###': 'h3', '####': 'h4', '#####': 'h5',
 
 var textBlock = function (txt) {
 	this.text = txt;
+	var _parsing = '';
+	this.html = '';
 };
 
-textBlock.prototype.replaceBasicSymbol = function(sym) {
+textBlock.prototype.parseBasicSymbol = function(sym) {
 
 	var regex = new RegExp('(?:[^\\' + sym + '\\\\]|\\\\.)*', 'g');
-	var chunks = this.text.match(regex);
+	var chunks = _parsing.match(regex);
 	if (chunks[chunks.length - 1] === '') {
 		chunks.pop();
 	}
@@ -28,28 +30,28 @@ textBlock.prototype.replaceBasicSymbol = function(sym) {
 	if (inTag) {
 		result += '</' + syntax[sym] + '>';
 	}
-	this.text = result;
+	_parsing = result;
 };
 
-textBlock.prototype.replaceLineSymbol = function(sym) {
+textBlock.prototype.parseLineSymbol = function(sym) {
 	
-	if (this.text === sym) {
-		this.text = '<' + lineSyntax[sym] + '>';
+	if (_parsing === sym) {
+		_parsing = '<' + lineSyntax[sym] + '>';
 		return true;
 	}
 	
 	return false;
 };
 
-textBlock.prototype.replaceHeaders = function () {
+textBlock.prototype.parseHeaders = function () {
 	
-	if (this.text.charAt(0) !== '#') {
+	if (_parsing.charAt(0) !== '#') {
 		return false;
 	}
 
 	for (var i = 1; i < 7; i++) {
-		if (this.text.charAt(i) !== '#' || i == 6) {
-			this .text = '<h' + i + '>' + this.text.substring(i) + '</h' + i + '>';
+		if (_parsing.charAt(i) !== '#' || i == 6) {
+			_parsing = '<h' + i + '>' + _parsing.substring(i) + '</h' + i + '>';
 			return true;
 		}
 	}
@@ -57,10 +59,10 @@ textBlock.prototype.replaceHeaders = function () {
 	return true;
 }
 
-textBlock.prototype.replaceLinks = function() {
+textBlock.prototype.parseLinks = function() {
 
 	var regex = new RegExp(/\[(.*?)\]\((.*?)\)/, 'g');
-	var links = regex.exec(this.text);
+	var links = regex.exec(_parsing);
 
 	if (links == null) {
 		return;
@@ -69,17 +71,37 @@ textBlock.prototype.replaceLinks = function() {
 	var result = '';
 	var prevIndex = 0;
 	while (links != null) {
-		result += this.text.substring(prevIndex, links['index']) + '<a href="' + links[2] + '" target="_blank">' + links[1] + '</a>';
+		result += _parsing.substring(prevIndex, links['index']) + '<a href="' + links[2] + '" target="_blank">' + links[1] + '</a>';
 		prevIndex = regex.lastIndex;
-		links = regex.exec(this.text);
+		links = regex.exec(_parsing);
 	}
 
-	this.text = result + this.text.substring(prevIndex);
+	_parsing = result + _parsing.substring(prevIndex);
 };
 
 textBlock.prototype.stripEscapes = function() {
-	this.text = this.text.replace(/\\/g, '');
+	_parsing = _parsing.replace(/\\/g, '');
 };
+
+textBlock.prototype.parse = function() {
+	
+	_parsing = this.text;
+
+	if (this.parseHeaders() || this.parseLineSymbol('==')) {
+		this.html = _parsing;
+		return;
+	}
+
+	this.parseLinks();
+
+	this.parseBasicSymbol('*');
+	this.parseBasicSymbol('_');
+	this.parseBasicSymbol('^');
+	
+	this.stripEscapes();
+
+	this.html = '<p>' + _parsing + '</p>';
+}
 
 function generateMarkdown(text) {
 
@@ -88,27 +110,10 @@ function generateMarkdown(text) {
 
 	//Process each line separately
 	for (var i = 0; i < lines.length; i++) {
-		output = output + processBlock(lines[i]);
+		var block = new textBlock(lines[i]);
+		block.parse();
+		output = output + block.html;
 	}
 
 	return output;
-}
-
-function processBlock(line) {
-
-	var block = new textBlock(line);
-	
-	if (block.replaceHeaders() || block.replaceLineSymbol('==')) {
-		return block.text;
-	}
-	
-	block.replaceLinks();
-	
-	block.replaceBasicSymbol('*');
-	block.replaceBasicSymbol('_');
-	block.replaceBasicSymbol('^');
-	
-	block.stripEscapes();
-	
-	return '<p>' + block.text + '</p>';
 }
